306ecad253e55a10a658ac2c98609c917bbd559b
jaehyo <chojh0621@gmail.com>
2022-12-14 11:39:19 +0000

    valid set split code

--- code/train.py
+++ code/train.py
@@ -17,17 +17,20 @@ from model import EAST
 import wandb
 import numpy as np
 import random
+from inference import do_inference
+from deteval import calc_deteval_metrics
+import json
 
 wandb.login()
 wandb.init(
     project='OCR_project',
     entity="aitech4_cv3",
-    name='base',
+    name='base(ICDAR+upstage)',
     config={
         'lr': 0.001,
-        'batch_size':12,
-        'epoch':4,
-        'seed': 42
+        'batch_size':24,
+        'epoch' : 200,
+        'seed' : 42,
     }
 )
 
@@ -46,20 +49,20 @@ def parse_args():
     parser = ArgumentParser()
 
     # Conventional args
-    parser.add_argument('--seed', type=int, default=config.seed, help='random seed (default: 42)')
+    parser.add_argument('--seed', type=int, default=42, help='random seed (default: 42)')
     parser.add_argument('--data_dir', type=str,
-                        default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml//input/data/ICDAR17_Korean'))
+                        default=os.environ.get('SM_CHANNEL_TRAIN', '/opt/ml/input/data'))
     parser.add_argument('--model_dir', type=str, default=os.environ.get('SM_MODEL_DIR',
-                                '/opt/ml/level2_dataannotation_cv-level2-cv-03/code/trained_models'))
+                                                                        'trained_models'))
 
     parser.add_argument('--device', default='cuda' if cuda.is_available() else 'cpu')
     parser.add_argument('--num_workers', type=int, default=4)   
 
     parser.add_argument('--image_size', type=int, default=1024)
     parser.add_argument('--input_size', type=int, default=512)
-    parser.add_argument('--batch_size', type=int, default=config.batch_size)
-    parser.add_argument('--learning_rate', type=float, default=config.lr)
-    parser.add_argument('--max_epoch', type=int, default=config.epoch)
+    parser.add_argument('--batch_size', type=int, default=12)
+    parser.add_argument('--learning_rate', type=float, default=1e-3)
+    parser.add_argument('--max_epoch', type=int, default=200)
     parser.add_argument('--save_interval', type=int, default=5)
 
     args = parser.parse_args()
@@ -69,34 +72,65 @@ def parse_args():
 
     return args
 
+def calc_score(gt_anno, pred_anno):
+    precision, recall, f1_score = 0, 0, 0
+    num_images = len(gt_anno['images'])
+    
+    for gt_image, pred_image in zip(sorted(gt_anno['images'].items()), sorted(pred_anno['images'].items())):
+        gt_bboxes_dict, pred_bboxes_dict, transcriptions_dict  = {}, {}, {}
+        gt_bboxes_list, pred_bboxes_list, transcriptions_list  = [], [], []
+        
+        for gt_point_index in gt_image[1]['words']:
+            gt_bboxes_list.extend([gt_image[1]['words'][str(gt_point_index)]['points']])
+            transcriptions_list.extend([gt_image[1]['words'][str(gt_point_index)]['transcription']])
+        
+        for pred_point_index in pred_image[1]['words']:
+            pred_bboxes_list.extend([pred_image[1]['words'][pred_point_index]['points']])
+        
+        gt_bboxes_dict[gt_image[0]] = gt_bboxes_list
+        transcriptions_dict[gt_image[0]] = transcriptions_list
+        pred_bboxes_dict[gt_image[0]] = pred_bboxes_list
+        
+        metrics = calc_deteval_metrics(pred_bboxes_dict=pred_bboxes_dict, gt_bboxes_dict=gt_bboxes_dict, transcriptions_dict=transcriptions_dict)
+        
+        precision += metrics['total']['precision']
+        recall += metrics['total']['recall']
+        f1_score += metrics['total']['hmean']
+    
+    return precision/num_images, recall/num_images, f1_score/num_images
 
 def do_training(seed, data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval):
     
     seed_everything(seed)
 
-    dataset = SceneTextDataset(data_dir, split='train', image_size=image_size, crop_size=input_size)
-    dataset = EASTDataset(dataset)
-    num_batches = math.ceil(len(dataset) / batch_size)
-    train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-
+    train_dataset = SceneTextDataset(f'{data_dir}/train_image', split='train', image_size=image_size, crop_size=input_size)
+    train_dataset = EASTDataset(train_dataset)
+    train_num_batches = math.ceil(len(train_dataset) / batch_size)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+    
+    valid_dataset = SceneTextDataset(f'{data_dir}/valid_image', split='valid', image_size=image_size, crop_size=input_size)
+    valid_dataset = EASTDataset(valid_dataset)
+    valid_num_batches = math.ceil(len(valid_dataset) / batch_size)
+    valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model = EAST()
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
 
-    model.train()
 
     mean_loss = float('inf')
+    max_f1 = 0
+    
     for epoch in range(max_epoch):
-        epoch_loss, epoch_start = 0, time.time()
-        cls_loss = float('inf')
-        angle_loss = float('inf')
-        iou_loss = float('inf')
-        with tqdm(total=num_batches) as pbar:
+        model.train()
+        epoch_loss, epoch_cls_loss, epoch_angle_loss, epoch_iou_loss, epoch_start = 0, 0, 0, 0, time.time()
+        
+        with tqdm(total=train_num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
-                pbar.set_description('[Epoch {}]'.format(epoch + 1))
+                pbar.set_description('[[train] Epoch {}]'.format(epoch + 1))
 
                 loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
                 optimizer.zero_grad()
@@ -105,48 +139,80 @@ def do_training(seed, data_dir, model_dir, device, image_size, input_size, num_w
 
                 loss_val = loss.item()
                 epoch_loss += loss_val
-
+                epoch_cls_loss += extra_info['cls_loss']
+                epoch_angle_loss += extra_info['angle_loss']
+                epoch_iou_loss += extra_info['iou_loss']
+                
                 pbar.update(1)
                 val_dict = {
-                    'Cls loss': extra_info['cls_loss'], 'Angle loss': extra_info['angle_loss'],
-                    'IoU loss': extra_info['iou_loss']
+                    'Cls loss': epoch_cls_loss/train_num_batches, 'Angle loss': epoch_angle_loss/train_num_batches,
+                    'IoU loss': epoch_iou_loss/train_num_batches
                 }
                 pbar.set_postfix(val_dict)
 
-                if cls_loss >= extra_info['cls_loss']:
-                    cls_loss = extra_info['cls_loss']
-                if angle_loss >= extra_info['angle_loss']:
-                    angle_loss = extra_info['angle_loss']
-                if iou_loss >= extra_info['iou_loss']:
-                    iou_loss = extra_info['iou_loss']
-
-                
-                
-
-                
+        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
+            epoch_loss / train_num_batches, timedelta(seconds=time.time() - epoch_start)))
 
         scheduler.step()
-
-        print('Mean loss: {:.4f} | Elapsed time: {}'.format(
-            epoch_loss / num_batches, timedelta(seconds=time.time() - epoch_start)))
-    
-        if mean_loss >= epoch_loss / num_batches:
-            mean_loss = epoch_loss / num_batches
-            if not osp.exists(model_dir):
-                os.makedirs(model_dir)
-
-            ckpt_fpath = osp.join(model_dir, 'latest.pth')
-            torch.save(model.state_dict(), ckpt_fpath)
-
+        
+        model.eval()
+        val_epoch_loss, val_epoch_cls_loss, val_epoch_angle_loss, val_epoch_iou_loss, val_epoch_start = 0, 0, 0, 0, time.time()
+        
+        with tqdm(total=valid_num_batches) as pbar:
+            for img, gt_score_map, gt_geo_map, roi_mask in valid_loader:
+                pbar.set_description('[[valid] Epoch {}]'.format(epoch + 1))
+                
+                with torch.no_grad():
+                    loss, extra_info = model.train_step(img, gt_score_map, gt_geo_map, roi_mask)
+                
+                loss_val = loss.item()
+                val_epoch_loss += loss_val
+                val_epoch_cls_loss += extra_info['cls_loss']
+                val_epoch_angle_loss += extra_info['angle_loss']
+                val_epoch_iou_loss += extra_info['iou_loss']
+                
+                pred_score_map = extra_info['score_map']
+                pred_geo_map = extra_info['geo_map']
+                
+                pbar.update(1)
+                val_dict = {
+                    'Cls loss': val_epoch_cls_loss/valid_num_batches, 'Angle loss': val_epoch_angle_loss/valid_num_batches,
+                    'IoU loss': val_epoch_iou_loss/valid_num_batches
+                }
+                pbar.set_postfix(val_dict)
+        
+        data_dir = '/opt/ml/input/data'
+        with open('/opt/ml/input/data/ufo/valid.json', 'r') as f:
+            gt_anno = json.load(f)
+        pred_anno = do_inference(model=model, data_dir=data_dir, ckpt_fpath=None, input_size=input_size, batch_size=valid_num_batches, split='valid_image')
+        precision, recall, f1_score = calc_score(gt_anno, pred_anno)
+        
+        if mean_loss >= val_epoch_loss / valid_num_batches:
+            mean_loss = val_epoch_loss / valid_num_batches
+            
         wandb.log({
                     'Mean_loss': mean_loss,
-                    'Cls_loss': cls_loss,
-                    'Angle_loss': angle_loss,
-                    'IoU_loss': iou_loss
+                    'Cls_loss': val_epoch_cls_loss/valid_num_batches,
+                    'Angle_loss': val_epoch_angle_loss/valid_num_batches,
+                    'IoU_loss': val_epoch_iou_loss/valid_num_batches,
+                    'precision' : precision,
+                    'recall' : recall,
+                    'f1_score' : f1_score
                     })
         
         wandb.watch(model)
-
+        
+        print('f1 score: {:.4f} | Elapsed time: {}'.format(
+            f1_score, timedelta(seconds=time.time() - val_epoch_start)))
+        
+        if max_f1 <= f1_score:
+            max_f1 = f1_score
+            if not osp.exists(model_dir):
+                os.makedirs(model_dir)
+            print(f"Update latest.pth! f1 score:{f1_score}")
+            ckpt_fpath = osp.join(model_dir, 'latest.pth')
+            torch.save(model.state_dict(), ckpt_fpath)
+            
 
 def main(args):
     do_training(**args.__dict__)
